c2060bea704341cc5f567ec9648864468fd575ee
x-jirox <correalenin045@gmail.com>
2025-11-25 23:56:16 -0500

    MeetingSetup page

--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,31 +1,49 @@
 'use client'
 
-import { useCall, VideoPreview } from '@stream-io/video-react-sdk'
+import { DeviceSettings, useCall, VideoPreview } from '@stream-io/video-react-sdk'
 import React, { useEffect, useState } from 'react'
 
-const MeetingSetup = () => {
+const MeetingSetup = ({setIsSetupComplete} : {setIsSetupComplete : (value : boolean) =>void}) => {
 
-    const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false)
-    const call = useCall();
+  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false)
+  const call = useCall();
 
-    if(!call){
-        throw new Error('usecall must be used within streamcall componrnt')
+  if (!call) {
+    throw new Error('usecall must be used within streamcall componrnt')
+  }
+
+  useEffect(() => {
+    if (isMicCamToggledOn) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.camera.enable();
+
     }
-
-    useEffect(() => {
-        if(isMicCamToggledOn){
-            call?.camera.disable();
-            call?.microphone.disable();
-        }else{
-            call?.camera.enable();
-            call?.camera.enable();
-
-        }
-    },[isMicCamToggledOn, call?.camera, call?.microphone])
+  }, [isMicCamToggledOn, call?.camera, call?.microphone])
   return (
     <div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
       <h1 className='text-2xl font-bold'>Setup</h1>
-      <VideoPreview/>
+      <VideoPreview />
+      <div className="flex h-screen w-full flex-col items-center justify-center gap-3">
+        <label className='flex items-center justify-center gap-2 font-medium'>
+          <input
+            type="checkbox"
+            checked={isMicCamToggledOn}
+            onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
+          />
+          mic and phone
+        </label>
+        <DeviceSettings />
+      </div>
+      <button className='rounded-md bg-green-500 px-4 py-2.5'
+      onClick={() =>{
+        call.join();
+        setIsSetupComplete(true)
+      }}>
+Ingresar
+      </button>
     </div>
   )
 }
